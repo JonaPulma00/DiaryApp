589ebb1f72bea7957a5a6affdf567b861470ebaf
Jonathan <jona.rierasanchez@gmail.com>
2026-01-13 11:02:05 +0100

    Creating services structure

---
+++ DiaryApp/Services/DiaryEntryService.cs
@@ -0,0 +1,24 @@
+﻿using DiaryApp.Data;
+using DiaryApp.Models;
+
+namespace DiaryApp.Services
+{
+    public class DiaryEntryService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DiaryEntryService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<DiaryEntry> GetAll()
+        {
+            var objDiaryEntryList = _db.DiaryEntries
+                .OrderByDescending(e => e.Created)
+                .ToList();
+
+            return objDiaryEntryList;   
+        }
+    }
+}
